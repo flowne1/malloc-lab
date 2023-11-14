65a0004e5c291120253b73af1648dde2e280c395
Ubuntu <ubuntu@ip-172-31-3-31.ap-northeast-2.compute.internal>
2023-11-14 02:00:11 +0000

    중간 커밋, cccp-bal.rep의 SF해결필요

--- mm.c
+++ mm.c
@@ -6,7 +6,8 @@
 // 디버거, 프로파일러 사용도 추천한다. gcc기준으로 컴파일할 때 -g, -pg 인자 사용
 // 일단 박고봐라!
 
-// 왜 포인터 참조를 통한 할당과 매크로를 통한 할당이 값이 차이가 나는가.............. 왜...........왜.......
+// 왜 포인터 참조를 통한 할당과 매크로를 통한 할당이 값이 차이가 나는가.............. 왜...........왜......
+// > 이거 값을 직접 넣는 코드 때문에, 데이터형이 안맞아서 생기는 문제인 것 같다. 데이터형을 맞추기 위해 기계적으로 매크로를 사용하자.
 
 
 #include <stdio.h>
@@ -64,6 +65,7 @@ static void *coalesce(void *bp);
 void *first_fit(size_t alloc_size);
 
 // 추가로 필요한 변수를 정의한다
+static char *heap_listp;
 
 static void *extend_heap(size_t words){
     char *bp;
@@ -71,7 +73,6 @@ static void *extend_heap(size_t words){
     // 받은 words를 토대로, size를 8바이트의 배수로 복원하고 그 크기만큼 할당한다
     // ex. 12바이트를 요청받은 경우 >> 16바이트로 사이즈를 복원하고 할당
     size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
-    printf("확장 사이즈는 : %i\n", size);
     // 포인터와 정수를 비교하기 위해 void*을 long으로 캐스팅한다. windows는 long long 써야할듯?
     if ((long)(bp = mem_sbrk(size)) == -1){
         return NULL; 
@@ -79,7 +80,7 @@ static void *extend_heap(size_t words){
     // 힙을 확장한 후 해당 블록의 헤더, 풋터의 정보들을 갱신한다
     PUT(HDRP(bp), PACK(size, 0));
     PUT(FTRP(bp), PACK(size, 0));
-    PUT(HDRP(NEXT_BLKP(bp)), 1); // 새로운 에필로그 헤더가 된다
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0,1)); // 새로운 에필로그 헤더가 된다
     // 이전 블록이 열려있으면 연결한 후 bp를 반환한다
     return coalesce(bp);
 }
@@ -89,17 +90,16 @@ static void *coalesce(void *bp){
     char* bp_prev = PREV_BLKP(bp);
     if (!GET_ALLOC(HDRP(bp_prev))){
         size_t new_size = GET_SIZE(HDRP(bp_prev)) + GET_SIZE(HDRP(bp));
-        printf("sizebp:%i, sizeprevbp:%i, new_size:%i\n", GET_SIZE(HDRP(bp)), GET_SIZE(HDRP(bp_prev)),new_size);
         PUT(HDRP(bp_prev), PACK(new_size, 0));
+        PUT(FTRP(bp_prev), PACK(new_size, 0)); // 풋터 갱신
         bp = bp_prev; // ptr을 이전 블록까지 땡겨놓는다. 뒤에 if문 걸릴때 블록 병합에 대비 + 리턴 대비
-        printf("ftrp : %p, hdrp : %p", FTRP(bp), HDRP(bp));
-        PUT(FTRP(bp), *HDRP(bp)); // 풋터 갱신
     }
     char* bp_next = NEXT_BLKP(bp);
     // 뒷 블록이 사용 가능하면 병합한다
     if (!GET_ALLOC(HDRP(bp_next))){
-        PUT(HDRP(bp), GET_SIZE(HDRP(bp)) + GET_SIZE(HDRP(bp_next)));
-        PUT(FTRP(bp), *HDRP(bp)); // 풋터 갱신
+        size_t new_size = GET_SIZE(HDRP(bp)) + GET_SIZE(HDRP(bp_next));
+        PUT(HDRP(bp), PACK(new_size, 0));
+        PUT(FTRP(bp), PACK(new_size, 0)); // 풋터 갱신
     }
     return bp;
 }
@@ -109,7 +109,7 @@ static void *coalesce(void *bp){
 int mm_init(void)
 {
     // 힙을 생성하고 초기 포인터를 반환받는다. 실패시 에러 메세지를 받는다.
-    char* heap_listp = mem_sbrk(4*WSIZE);
+    heap_listp = mem_sbrk(4*WSIZE);
     if (heap_listp == (void*)-1){
         return -1;
     }
@@ -117,7 +117,7 @@ int mm_init(void)
     PUT(heap_listp + WSIZE*0, 0); // 첫번째 워드는 정렬을 맞추기 위해 0을 박는다
     PUT(heap_listp + WSIZE*1, PACK(DSIZE, 1)); // 두번째 워드는 프롤로그 헤더. 사이즈 + 할당정보 
     PUT(heap_listp + WSIZE*2, PACK(DSIZE, 1)); // 세번째 워드는 프롤로그 풋터. 사이즈 + 할당정보
-    PUT(heap_listp + WSIZE*3, 1); // 네번째 워드는 에필로그 헤더. 사이즈는 0이고 할당정보만 넣는다
+    PUT(heap_listp + WSIZE*3, PACK(0,1)); // 네번째 워드는 에필로그 헤더. 사이즈는 0이고 할당정보만 넣는다
 
     // 힙을 확장
     if (extend_heap(HEAP_CHUNK_SIZE/WSIZE) == NULL){
@@ -129,19 +129,16 @@ int mm_init(void)
 // first-fit 방식의 할당을 구현한다. 비어있는 bp를 반환한다
 void *first_fit(size_t alloc_size){
     // 힙 시작점을 받아온다
-    char *bp = mem_heap_lo() + 16; // 패딩 4바이트, 프롤로그 8바이트, 헤더 4바이트 .. 시작점에서 16바이트 뒤에 첫 bp가 있다
+    char *bp = mem_heap_lo() + 4*WSIZE; // 패딩 4바이트, 프롤로그 8바이트, 헤더 4바이트 .. 시작점에서 16바이트 뒤에 첫 bp가 있다
     while(GET_SIZE(HDRP(bp)) != 0){
-        printf("현재포인터 : %p, size : %i, alloc : %i\n", bp, GET_SIZE(HDRP(bp)), GET_ALLOC(HDRP(bp)));
         // 할당되어있지않고, 사이즈가 충분한 경우
         if (!GET_ALLOC(HDRP(bp)) && GET_SIZE(HDRP(bp)) >= alloc_size){
-            printf("찾았습니다!\n");
             return bp;
         }
         // 조건을 만족하지 못하면 다음 블록으로 넘어간다
         bp = NEXT_BLKP(bp);
     }
     // 못찾았으면 void*(-1)을 반환한다
-    printf("firstfit 실패!\n");
     return (void*)-1;
 }
 
@@ -149,19 +146,18 @@ void *first_fit(size_t alloc_size){
 // 메모리를 할당하고 포인터를 반환한다
 void *mm_malloc(size_t size){
     // 헤더+풋터(=8바이트)를 포함해서 필요한 사이즈를 정의한다
-    int new_size = ALIGN(size + SIZE_T_SIZE);
-    printf("\n필요한 사이즈 : %i\n", new_size);
-    
+    size_t new_size = ALIGN(size + SIZE_T_SIZE);
     // first fit : 힙의 처음부터 끝까지 스프린트 하면서 가능한 공간을 찾는다
-    printf("스프린트 시작\n");
     char *bp = first_fit(new_size);
     // 가용 메모리가 부족하면 힙을 확장한다
     if (bp == (void*)-1){
-        printf("힙을 확장합니다!\n\n");
+        printf("\n확장중!\n");
         bp = extend_heap(new_size/WSIZE); // 여기 확장하는 사이즈 나중에 다시 볼것!
     }
     // bp를 기준으로 할당처리를한다
     size_t remain_size = GET_SIZE(HDRP(bp)) - new_size; // 여기 데이터 타입이 size_t가 맞을까..?
+    printf("\ngetsize : %zu, new_size : %zu \n", GET_SIZE(HDRP(bp)), new_size);
+    printf("bp : %p, hdrp : %p, remain_size : %zu\n", bp, HDRP(bp), remain_size);
     // 헤더, 풋터에 할당, 사이즈 정보를 넣는다
     PUT(HDRP(bp), PACK(new_size, 1));
     PUT(FTRP(bp), PACK(new_size, 1));
@@ -181,15 +177,12 @@ void mm_free(void *bp){
     if (!GET_ALLOC(HDRP(bp))){
         exit(EXIT_FAILURE);
     }
-    printf("\n해제합니다!!\n");
-    printf("bp : %p, size : %i\n", bp, GET_SIZE(HDRP(bp)));
     // 할당된 상태면 할당정보인 1을 헤더, 풋터에서 제거해준다
     PUT(HDRP(bp), PACK(GET_SIZE(HDRP(bp)), 0));
     PUT(FTRP(bp), PACK(GET_SIZE(FTRP(bp)), 0));
 
     // 앞뒤 블록을 병합한다
     coalesce(bp);
-    printf("해제 및 병합 완료\n");
     return;
 }
 
