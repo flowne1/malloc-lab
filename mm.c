0f64764e3b5a9aab9ed762a9160822b84354fa43
Ubuntu <ubuntu@ip-172-31-3-31.ap-northeast-2.compute.internal>
2023-11-15 14:13:44 +0000

    중간 커밋

--- mm.c
+++ mm.c
@@ -1,14 +1,6 @@
-// 들어가기전에. 정글 & CMU에서 얘기하는 팁
-// csapp 9.9장을 참고할 것
-// make시에 아래와 같은 인자를 추가로 사용할 수 있다
-//  -v, V : 각 trace 파일에 대한 요약을 확인할 수 있음
-//  -f : 특정 트레이스 파일만 디버깅 용으로 집어넣어볼 수 있다. short1-bal.rep, short2-bal.rep 사용해볼 것
-// 디버거, 프로파일러 사용도 추천한다. gcc기준으로 컴파일할 때 -g, -pg 인자 사용
-// 일단 박고봐라!
-
-// 왜 포인터 참조를 통한 할당과 매크로를 통한 할당이 값이 차이가 나는가.............. 왜...........왜......
-// > 이거 값을 직접 넣는 코드 때문에, 데이터형이 안맞아서 생기는 문제인 것 같다. 데이터형을 맞추기 위해 기계적으로 매크로를 사용하자.
-
+// Free block tracking : explicit
+// Insertion policy of free list : LIFO
+// Allocating policy : first-fit
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -52,20 +44,59 @@ team_t team = {
 // 헤더(또는 풋터)의 포인터를 통해 블록의 사이즈와 할당정보를 알 수 있는 단순한 매크로
 #define GET_SIZE(p) (GET(p) & ~0x7)
 #define GET_ALLOC(p) (GET(p) & 0x1)
-// 메모리블록의 포인터 bp를 통해 헤더, 풋터의 포인터를 찾는 매크로
+// 메모리블록의 포인터 bp를 통해 헤더, 풋터의 포인터를 찾는 매크로.
 #define HDRP(bp) ((char*)(bp) - WSIZE)
 #define FTRP(bp) ((char*)(bp) + GET_SIZE((char*)(bp) - WSIZE) - DSIZE)
 // 다음, 이전 bp를 찾는 매크로를 지정한다. 각각 헤더와 풋터에 있는 사이즈 정보를 이용하는것.
 #define NEXT_BLKP(bp) ((char*)(bp) + GET_SIZE((char*)(bp) - WSIZE))
 #define PREV_BLKP(bp) ((char*)(bp) - GET_SIZE((char*)(bp) - DSIZE))
+// bp를 통해 next free block, prev free block을 찾는 매크로를 정의한다
+#define NEXT_FBLKP(bp) (*(char**)(bp)) // char* 포인터(=bp)를 가리키는 포인터를 역참조하여 char* bp를 받아온다
+#define PREV_FBLKP(bp) (*(char**)((bp) + WSIZE))
+
 
 // 추가로 필요한 함수를 정의한다
 static void *extend_heap(size_t words);
 static void *coalesce(void *bp);
 void *first_fit(size_t alloc_size);
+void connect_prev_next(char *bp);
+void add_freeblock(char *bp);
 
 // 추가로 필요한 변수를 정의한다
 static char *heap_listp;
+static char *first_fbp;
+
+// bp를 기준으로, 앞뒤의 free 블록을 연결한다
+void connect_prev_next(char *bp){
+    // 만약 이미 할당된 블록의 bp를 받았으면 따로 처리하지 않는다
+    if (GET_ALLOC(HDRP(bp))){
+        return;
+    }
+    // 앞뒤의 가용블록을 연결한다.
+    char *next_fbp = NEXT_FBLKP(bp);
+    char *prev_fbp = PREV_FBLKP(bp);
+    if (prev_fbp){
+        NEXT_FBLKP(prev_fbp) = next_fbp; // 앞에 가용 블록이 있으면, 이전 가용 블록의 next로 next_fbp를 넣는다
+    }else{
+        first_fbp = next_fbp; // 앞에 가용 블록이 없는 경우, next_fbp가 first_fbp가 된다
+    }
+    if (next_fbp){
+        PREV_FBLKP(next_fbp) = prev_fbp; // 뒤에 가용 블록이 있으면, 다음 가용 블록의 prev로 prev_fbp를 넣는다
+    }
+    return;
+}
+
+// LIFO 방식의 신규 가용블록 처리
+void add_freeblock(char *bp){
+    // first fbp가 있으면 bp의 뒤에 연결한다
+    if (first_fbp){
+        PREV_FBLKP(first_fbp) = bp;
+    }
+    // bp를 first_fbp로 만드는 작업
+    PREV_FBLKP(bp) = NULL;
+    NEXT_FBLKP(bp) = first_fbp;
+    first_fbp = bp;
+}
 
 // 힙을 확장. 인자를 word단위로 받아야함
 static void *extend_heap(size_t words){
@@ -74,7 +105,7 @@ static void *extend_heap(size_t words){
     // 받은 words를 토대로, size를 8바이트의 배수로 복원하고 그 크기만큼 할당한다
     // ex. 12바이트를 요청받은 경우 >> 16바이트로 사이즈를 복원하고 할당
     size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
-    // 포인터와 정수를 비교하기 위해 void*을 long으로 캐스팅한다. windows는 long long 써야할듯?
+    // 포인터와 정수를 비교하기 위해 void*을 long으로 캐스팅한다.
     if ((long)(bp = mem_sbrk(size)) == -1){
         return NULL; 
     }
@@ -94,6 +125,7 @@ static void *coalesce(void *bp){
         size_t new_size = GET_SIZE(HDRP(bp_prev)) + GET_SIZE(HDRP(bp));
         PUT(HDRP(bp_prev), PACK(new_size, 0));
         PUT(FTRP(bp_prev), PACK(new_size, 0)); // 풋터 갱신
+        connect_prev_next(bp_prev); // bp_prev의 앞뒤를 연결한다
         bp = bp_prev; // ptr을 이전 블록까지 땡겨놓는다. 뒤에 if문 걸릴때 블록 병합에 대비 + 리턴 대비
     }
     char* bp_next = NEXT_BLKP(bp);
@@ -102,10 +134,10 @@ static void *coalesce(void *bp){
         size_t new_size = GET_SIZE(HDRP(bp)) + GET_SIZE(HDRP(bp_next));
         PUT(HDRP(bp), PACK(new_size, 0));
         PUT(FTRP(bp), PACK(new_size, 0)); // 풋터 갱신
+        connect_prev_next(bp_next); // bp_next의 앞뒤를 연결한다
     }
     return bp;
 }
-
 
 // 힙에 사전에 정의된 크기의 메모리를 할당하고 초기화한다
 int mm_init(void)
@@ -117,28 +149,33 @@ int mm_init(void)
     }
     // 처음 힙을 생성할 때, 필요한 작업을 한다
     PUT(heap_listp + WSIZE*0, 0); // 첫번째 워드는 정렬을 맞추기 위해 0을 박는다
-    PUT(heap_listp + WSIZE*1, PACK(DSIZE, 1)); // 두번째 워드는 프롤로그 헤더. 사이즈 + 할당정보 
-    PUT(heap_listp + WSIZE*2, PACK(DSIZE, 1)); // 세번째 워드는 프롤로그 풋터. 사이즈 + 할당정보
+    PUT(heap_listp + WSIZE*1, PACK(2*WSIZE, 1)); // 두번째 워드는 프롤로그 헤더. 사이즈 + 할당정보 
+    PUT(heap_listp + WSIZE*2, PACK(2*WSIZE, 1)); // 세번째 워드는 프롤로그 풋터. 사이즈 + 할당정보
     PUT(heap_listp + WSIZE*3, PACK(0,1)); // 네번째 워드는 에필로그 헤더. 사이즈는 0이고 할당정보만 넣는다
 
     // 힙을 확장
     if (extend_heap(HEAP_CHUNK_SIZE/WSIZE) == NULL){
         return -1;
     }
+    // first_fbp를 지정한다
+    first_fbp = heap_listp + 4*WSIZE;
+    // // 새 가용리스트 NEXT, PREV 정보를 갱신한다
+    // NEXT_FBLKP(first_fbp) = NULL;
+    // PREV_FBLKP(first_fbp) = NULL;
     return 0;
 }
 
 // first-fit 방식의 할당을 구현한다. 비어있는 bp를 반환한다
 void *first_fit(size_t alloc_size){
     // 힙 시작점을 받아온다
-    char *bp = mem_heap_lo() + 4*WSIZE; // 패딩 4바이트, 프롤로그 8바이트, 헤더 4바이트 .. 시작점에서 16바이트 뒤에 첫 bp가 있다
-    while(GET_SIZE(HDRP(bp)) != 0){
-        // 할당되어있지않고, 사이즈가 충분한 경우
-        if (!GET_ALLOC(HDRP(bp)) && GET_SIZE(HDRP(bp)) >= alloc_size){
+    char *bp = first_fbp;
+    while(bp != NULL){
+        // 사이즈가 충분한 경우 해당 bp를 반환한다
+        if (GET_SIZE(HDRP(bp)) >= alloc_size){
             return bp;
         }
         // 조건을 만족하지 못하면 다음 블록으로 넘어간다
-        bp = NEXT_BLKP(bp);
+        bp = NEXT_FBLKP(bp);
     }
     // 못찾았으면 NULL을 반환한다
     return NULL;
@@ -148,7 +185,7 @@ void *first_fit(size_t alloc_size){
 void *mm_malloc(size_t size){
     // 헤더+풋터(=8바이트)를 포함해서 필요한 사이즈를 정의한다
     size_t new_size = ALIGN(size + SIZE_T_SIZE);
-    // first fit : 힙의 처음부터 끝까지 스프린트 하면서 가능한 공간을 찾는다
+    // first fit : 힙의 처음부터 끝까지, "가용 메모리 리스트만" 스프린트 하면서 가능한 공간을 찾는다
     char *bp = first_fit(new_size);
     // 가용 메모리가 부족하면 힙을 확장한다
     if (bp == NULL){
@@ -157,25 +194,30 @@ void *mm_malloc(size_t size){
             return NULL;
         }
     }
-
     // bp를 기준으로 할당처리를한다
     size_t block_size = GET_SIZE(HDRP(bp));
     size_t remain_size = block_size - new_size; // 여기 데이터 타입이 size_t가 맞을까..?
     // 남는 블록이 16바이트이상(헤더,풋터 포함 데이터를 가질 수 있는 최소값)이면 block_size를 분할한다
     if (remain_size >= 16){
+        printf("여기\n");
         // 할당하는 블록의 정보를 갱신한다
         PUT(HDRP(bp), PACK(new_size,1));
         PUT(FTRP(bp), PACK(new_size,1));
+        connect_prev_next(bp);
         // 남은 블록의 정보를 갱신한다
         void *bp_next = NEXT_BLKP(bp);
         PUT(HDRP(bp_next), PACK(remain_size, 0));
         PUT(FTRP(bp_next), PACK(remain_size, 0));
+        add_freeblock(bp_next);
     // 남는 블록이 16바이트미만인 경우, 남는 블록까지 합쳐서 전체를 다 사용한다
     }else{
+        printf("저기\n");
+        connect_prev_next(bp);
         PUT(HDRP(bp), PACK(block_size,1));
-        PUT(FTRP(bp), PACK(block_size,1));        
+        PUT(FTRP(bp), PACK(block_size,1));
     }
     // 포인터를 반환한다
+    printf("%p 할당합니다\n", bp);
     return bp;
 }
 
@@ -191,6 +233,9 @@ void mm_free(void *bp){
 
     // 앞뒤 블록을 병합한다
     coalesce(bp);
+
+    // 가용리스트의 첫 지점에 할당해제한 블록을 추가한다(LIFO)
+    add_freeblock(bp);
     return;
 }
 
